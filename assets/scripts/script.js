8130229eab77c08149846212bf05c3219d02d9bb
Dazai13 <dazai1317@mail.ru>
2025-04-18 06:10:23 +0700

    Готовая адаптировання шапка с меню

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,2 @@
-const toggleBtn = document.getElementById('menu-toggle');
-const menu = document.querySelector('.mobile-menu');
-const icon = document.getElementById('menu-icon');
-const burgerIcon = 'assets/images/burger.svg';
-const closeIcon = 'assets/images/close.svg';
-toggleBtn.addEventListener('click', () => {
-  const isOpen = !menu.classList.contains('hidden');
-  if (isOpen) {
-    menu.classList.add('hidden');
-    icon.src = burgerIcon;
-  } else {
-    menu.classList.remove('hidden');
-    icon.src = closeIcon;
-  }
-});
+/*меню*/
+document.addEventListener("DOMContentLoaded",(function(){const e=document.getElementById("menu-toggle"),t=document.querySelector(".mobile-menu");e.addEventListener("click",(()=>{t.classList.toggle("active")}))}));
